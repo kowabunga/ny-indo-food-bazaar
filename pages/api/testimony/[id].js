24ee8651353e45b3f008539f0357216377799f44
Anthony Siletti <anthony.siletti@gmail.com>
2021-04-30 18:27:49 -0400

    working on testimony routes

--- pages/api/testimony/[id].js
+++ pages/api/testimony/[id].js
@@ -21,6 +21,7 @@ export default async function handler(req, res) {
     return res.status(200).json(testimony);
   }
 
+  //@TODO - get rid of uuid and just work with mongodb id
   // Delete testimony (admin only)
   if (method === 'DELETE') {
     const testimony = await Testimony.findById({ _id: id });
