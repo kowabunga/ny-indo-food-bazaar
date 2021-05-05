430466ca39f3207b09084ad22f2e02e31c859347
Anthony Siletti <anthony.siletti@gmail.com>
2021-05-04 20:01:56 -0400

    home page work

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
     <section className={`w-full my-3 grid grid-cols-6`}>
-      <div className='col-start-1 col-end-4 md:col-start-2 md:col-end-6 p-3 mt-5'>
-        <h1 className='text-5xl md:text-6xl lg:text-7xl text-white'>
+      <div className='col-start-1 col-end-7 md:col-start-2 md:col-end-6 p-3 mt-5 md:text-center'>
+        <h1 className='text-5xl md:text-6xl lg:text-6xl xl:text-7xl text-white tracking-wide'>
           New York Indonesian Food Bazaar
         </h1>
-        <p className='text-gray-200 lg:text-2xl'>
+        <p className='text-gray-200 lg:text-xl xl:text-2xl mt-2'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
           minima earum sit fuga vero repellendus suscipit quasi consequatur
           omnis numquam!
--- components/layout/nextEventDate.js
+++ components/layout/nextEventDate.js
@@ -1,6 +1,6 @@
 export default function NextEvent() {
   return (
-    <div className='rounded text-center mx-auto w-4/5 mt-20 p-3 text-2xl bg-yellow-100 text-gray-700 border-2 border-yellow-400'>
+    <div className='rounded text-center mx-auto w-4/5 lg:w-3/5 mt-20 p-3 text-2xl bg-green-100 text-gray-700 border-2 border-green-400'>
       <p className='inline-block'>Join us at our next event on</p>{' '}
       <date>{new Date().toLocaleString('en-us', { dateStyle: 'full' })}</date>
     </div>
